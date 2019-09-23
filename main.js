24bdf878a02b6498c5bbff8a103d26a6f582751c
bogdan <bogdandihtyar@gmail.com>
2019-09-23 15:14:49 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const score = document.querySelector('.score'),
+    start = document.querySelector('.start'),
+    gameArea = document.querySelector('.gameArea');
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false
+} 
+
+function startGame () {
+    start.classList.add('hide');
+}
+
+function startRun(event) {
+    event.preventDefault();
+    console.log(event.key);
+}
+
+function stopRun(event) {
+    event.preventDefault();
+}
